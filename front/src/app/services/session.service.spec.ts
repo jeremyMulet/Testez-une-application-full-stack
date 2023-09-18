ccbdf7404a4a6aa0eaf2222511658d42b2e6f129
jmulet <mulet.jeremy@gmail.com>
2023-09-18 21:51:58 +0200

    Add unit tests for session and user services

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,9 +2,19 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import {SessionInformation} from "../interfaces/sessionInformation.interface";
 
 describe('SessionService', () => {
   let service: SessionService;
+  let mockUser: SessionInformation = {
+    admin: false,
+    firstName: "",
+    id: 0,
+    lastName: "",
+    token: "",
+    type: "",
+    username: ""
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
@@ -13,5 +23,27 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should login the user', (done) => {
+    service.$isLogged().subscribe(isLogged => {
+      expect(isLogged).toBe(true);
+      done();
+    });
+
+    service.logIn(mockUser);
+    expect(service.sessionInformation).toEqual(mockUser);
+  });
+
+  it('should logout the user', (done) => {
+    service.logIn(mockUser); // first log in
+
+    service.$isLogged().subscribe(isLogged => {
+      expect(isLogged).toBe(false);
+      done();
+    });
+
+    service.logOut();
+    expect(service.sessionInformation).toBeUndefined();
   });
 });
--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -3,20 +3,54 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { UserService } from './user.service';
+import {User} from "../interfaces/user.interface";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Ensure that there are no outstanding requests
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should get user by his id', () => {
+    const mockUser: User = {
+      admin: false, createdAt: new Date(), email: "", firstName: "", lastName: "", password: "", id: 4
+    };
+    const userId = '4'
+
+    service.getById(userId).subscribe( user => {
+      expect(user).toEqual(mockUser);
+    });
+
+    const request = httpMock.expectOne(`api/user/${userId}`);
+    expect(request.request.method).toBe('GET');
+    request.flush(mockUser);
+  });
+
+  it('should delete user by id', () => {
+    const userId = '4'
+
+    service.delete(userId).subscribe(response => {
+      expect(response).toBe(null);
+    });
+
+    const request = httpMock.expectOne(`api/user/${userId}`);
+    expect(request.request.method).toBe('DELETE');
+    request.flush(null);
+  });
+
 });
