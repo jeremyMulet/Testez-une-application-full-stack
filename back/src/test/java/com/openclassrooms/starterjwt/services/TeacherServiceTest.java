937cf6a78ec8455f0df440e45896db686410933c
jmulet <mulet.jeremy@gmail.com>
2023-09-28 09:20:37 +0200

    Add backend services unit tests

---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/TeacherServiceTest.java
@@ -0,0 +1,76 @@
+package com.openclassrooms.starterjwt.services;
+
+import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.repository.TeacherRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Jérémy MULET on 28/09/2023.
+ */
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class TeacherServiceTest {
+
+
+    private TeacherService teacherService;
+
+    @Mock
+    private TeacherRepository teacherRepository;
+
+    @BeforeEach
+    public void setUp() {
+        teacherService = new TeacherService(teacherRepository);
+    }
+
+    @Test
+    public void testFindAll() {
+        Teacher teacher1 = new Teacher();
+        teacher1.setId(1L);
+        Teacher teacher2 = new Teacher();
+        teacher2.setId(2L);
+
+        when(teacherRepository.findAll()).thenReturn(Arrays.asList(teacher1, teacher2));
+
+        List<Teacher> teachers = teacherService.findAll();
+
+        assertThat(teachers).hasSize(2)
+                .contains(teacher1, teacher2);
+    }
+
+    @Test
+    public void testFindById_found() {
+        Teacher teacher = new Teacher();
+        teacher.setId(1L);
+
+        when(teacherRepository.findById(1L)).thenReturn(Optional.of(teacher));
+
+        Teacher foundTeacher = teacherService.findById(1L);
+
+        assertThat(foundTeacher).isNotNull().isEqualToComparingFieldByField(teacher);
+    }
+
+    @Test
+    public void testFindById_notFound() {
+        when(teacherRepository.findById(1L)).thenReturn(Optional.empty());
+
+        Teacher foundTeacher = teacherService.findById(1L);
+
+        assertThat(foundTeacher).isNull();
+    }
+}
+
