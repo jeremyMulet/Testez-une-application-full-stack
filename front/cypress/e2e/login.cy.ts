c0ddca99e537f6312eca0d5e0bb3a11cd8b866f2
jmulet <mulet.jeremy@gmail.com>
2023-10-16 17:55:55 +0200

    improve login and register cypress tests

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,60 +1,41 @@
-describe('Login spec', () => {
-  it('Login successfull', () => {
-    cy.visit('/login')
+export function run(){
+  describe('Login spec', () => {
+    it('Login successfull', () => {
+      cy.visit('/login')
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
-    })
+      cy.get('input[formControlName=email]').type("yoga@studio.com")
+      cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []).as('session')
-
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-
-    cy.url().should('include', '/sessions')
-  });
-
-  it('Login failed wrong password', () => {
-    cy.visit('/login');
-
-    cy.intercept('POST', '/api/auth/login', {
-      statusCode: 401,
+      cy.url().should('include', '/sessions')
     });
 
-    cy.get('input[formControlName=email]').type("yoga@studio.com");
-    cy.get('input[formControlName=password]').type("test!123");
-    cy.get('button[type=submit]').click();
+    it('Login failed wrong password', () => {
+      cy.visit('/login');
 
-    cy.url().should('include', '/login');
-    cy.contains('An error occurred').should('be.visible');
+      cy.get('input[formControlName=email]').type("yoga@studio.com");
+      cy.get('input[formControlName=password]').type("test!123");
+      cy.get('button[type=submit]').click();
+
+      cy.url().should('include', '/login');
+      cy.contains('An error occurred').should('be.visible');
+    });
+
+    it('Login failed no email', () => {
+      cy.visit('/login');
+
+      cy.get('input[formControlName=password]').type("test!123");
+
+      cy.get('button[type=submit]').should('be.disabled');
+      cy.url().should('include', '/login');
+    });
+
+    it('Login failed no password', () => {
+      cy.visit('/login');
+
+      cy.get('input[formControlName=email]').type("yoga@studio.com");
+
+      cy.get('button[type=submit]').should('be.disabled');
+      cy.url().should('include', '/login');
+    });
   });
-
-  it('Login failed no email', () => {
-    cy.visit('/login');
-
-    cy.get('input[formControlName=password]').type("test!123");
-
-    cy.get('button[type=submit]').should('be.disabled');
-    cy.url().should('include', '/login');
-  });
-
-  it('Login failed no password', () => {
-    cy.visit('/login');
-
-    cy.get('input[formControlName=email]').type("yoga@studio.com");
-
-    cy.get('button[type=submit]').should('be.disabled');
-    cy.url().should('include', '/login');
-  });
-});
+}
--- front/cypress/e2e/register.cy.ts
+++ front/cypress/e2e/register.cy.ts
@@ -1,80 +1,77 @@
-describe('register spec', () => {
-  it('Register successfully', () => {
-    cy.visit('/register')
+export function run(){
+  describe('register spec', () => {
+    it('Register successfully', () => {
+      cy.visit('/register')
 
-    cy.intercept('POST', '/api/auth/register', {
-      body: {"message":"User registered successfully!"},
+      cy.intercept('POST', '/api/auth/register', {
+        body: {"message":"User registered successfully!"},
+      })
+
+      cy.get('input[formControlName=firstName]').type("Toto")
+      cy.get('input[formControlName=lastName]').type('Toto')
+      cy.get('input[formControlName=email]').type('test@test.com')
+      cy.get('input[formControlName=password]').type('test!1234')
+      cy.get('button[type=submit]').click()
+
+      cy.url().should('include', '/login')
+
     })
 
-    cy.get('input[formControlName=firstName]').type("Toto")
-    cy.get('input[formControlName=lastName]').type('Toto')
-    cy.get('input[formControlName=email]').type('toto@test.com')
-    cy.get('input[formControlName=password]').type('test!1234')
-    cy.get('button[type=submit]').click()
+    it('Register failed email already taken', () => {
+      cy.visit('/register')
 
-    cy.url().should('include', '/login')
+      cy.get('input[formControlName=firstName]').type("Toto")
+      cy.get('input[formControlName=lastName]').type('Toto')
+      cy.get('input[formControlName=email]').type('yoga@studio.com')
+      cy.get('input[formControlName=password]').type('test!1234')
+      cy.get('button[type=submit]').click()
 
-  })
-
-  it('Register failed email already taken', () => {
-    cy.visit('/register')
-
-    cy.intercept('POST', '/api/auth/register', {
-      statusCode: 400,
-      body: {"message":"Error: Email is already taken!"},
+      cy.contains('An error occurred').should('be.visible')
+      cy.url().should('include', '/register')
     })
 
-    cy.get('input[formControlName=firstName]').type("Toto")
-    cy.get('input[formControlName=lastName]').type('Toto')
-    cy.get('input[formControlName=email]').type('toto@test.com')
-    cy.get('input[formControlName=password]').type('test!1234')
-    cy.get('button[type=submit]').click()
+    it('Register firstName empty', () => {
+      cy.visit('/register')
 
-    cy.contains('An error occurred').should('be.visible')
-    cy.url().should('include', '/register')
+      cy.get('input[formControlName=lastName]').type('Toto')
+      cy.get('input[formControlName=email]').type('toto@test.com')
+      cy.get('input[formControlName=password]').type('test!1234')
+
+      cy.get('button[type=submit]').should('be.disabled')
+      cy.url().should('include', '/register')
+    })
+
+    it('Register lastName empty', () => {
+      cy.visit('/register')
+
+      cy.get('input[formControlName=firstName]').type('Toto')
+      cy.get('input[formControlName=email]').type('toto@test.com')
+      cy.get('input[formControlName=password]').type('test!1234')
+
+      cy.get('button[type=submit]').should('be.disabled')
+      cy.url().should('include', '/register')
+    })
+
+    it('Register email empty', () => {
+      cy.visit('/register')
+
+      cy.get('input[formControlName=firstName]').type('Toto')
+      cy.get('input[formControlName=lastName]').type('Toto')
+      cy.get('input[formControlName=password]').type('test!1234')
+
+      cy.get('button[type=submit]').should('be.disabled')
+      cy.url().should('include', '/register')
+    })
+
+    it('Register password empty', () => {
+      cy.visit('/register')
+
+      cy.get('input[formControlName=firstName]').type('Toto')
+      cy.get('input[formControlName=lastName]').type('Toto')
+      cy.get('input[formControlName=email]').type('toto@test.com')
+
+      cy.get('button[type=submit]').should('be.disabled')
+      cy.url().should('include', '/register')
+    })
   })
-
-  it('Register firstName empty', () => {
-    cy.visit('/register')
-
-    cy.get('input[formControlName=lastName]').type('Toto')
-    cy.get('input[formControlName=email]').type('toto@test.com')
-    cy.get('input[formControlName=password]').type('test!1234')
-
-    cy.get('button[type=submit]').should('be.disabled')
-    cy.url().should('include', '/register')
-  })
-
-  it('Register lastName empty', () => {
-    cy.visit('/register')
-
-    cy.get('input[formControlName=firstName]').type('Toto')
-    cy.get('input[formControlName=email]').type('toto@test.com')
-    cy.get('input[formControlName=password]').type('test!1234')
-
-    cy.get('button[type=submit]').should('be.disabled')
-    cy.url().should('include', '/register')
-  })
-
-  it('Register email empty', () => {
-    cy.visit('/register')
-
-    cy.get('input[formControlName=firstName]').type('Toto')
-    cy.get('input[formControlName=lastName]').type('Toto')
-    cy.get('input[formControlName=password]').type('test!1234')
-
-    cy.get('button[type=submit]').should('be.disabled')
-    cy.url().should('include', '/register')
-  })
-
-  it('Register password empty', () => {
-    cy.visit('/register')
-
-    cy.get('input[formControlName=firstName]').type('Toto')
-    cy.get('input[formControlName=lastName]').type('Toto')
-    cy.get('input[formControlName=email]').type('toto@test.com')
-
-    cy.get('button[type=submit]').should('be.disabled')
-    cy.url().should('include', '/register')
-  })
-})
+}
